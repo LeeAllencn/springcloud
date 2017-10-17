bcc96745aa2f2e8aa962c459ca52cd98f5280e9a
lixin <lixin@hnair.com>
2017-10-17 11:22:50 +0800

    zipkin trace

---
+++ microservice-simple-provider-user-trace-zipkin/src/main/java/com/rocky/cloud/MicroserviceSimpleProviderUserTraceZipkinApplication.java
@@ -0,0 +1,12 @@
+package com.rocky.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSimpleProviderUserTraceZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSimpleProviderUserTraceZipkinApplication.class, args);
+	}
+}
