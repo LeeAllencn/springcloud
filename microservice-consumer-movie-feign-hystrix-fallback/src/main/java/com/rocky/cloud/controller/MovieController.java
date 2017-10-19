7e9df82ca300e3e1fe0e357759cc39df0b123901
lixin <lixin@hnair.com>
2017-10-19 20:17:05 +0800

    feign hystrix fallback

---
+++ microservice-consumer-movie-feign-hystrix-fallback/src/main/java/com/rocky/cloud/controller/MovieController.java
@@ -0,0 +1,22 @@
+package com.rocky.cloud.controller;
+
+import com.rocky.cloud.entity.User;
+import com.rocky.cloud.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Rocky on 2017-10-19.
+ */
+@RestController
+public class MovieController {
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.userFeignClient.findById(id);
+    }
+}
